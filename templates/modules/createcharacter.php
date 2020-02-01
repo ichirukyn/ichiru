ec677d277c4344ce69d65ff5454b44d5f9fc924f
ichirukyn <55537819+ichirukyn@users.noreply.github.com>
2020-02-01 13:39:28 +0300

    addcharacter

--- templates/modules/createcharacter.php
+++ templates/modules/createcharacter.php
@@ -4,10 +4,53 @@
 	<link rel="stylesheet" type="text/css" href="/css/styles.css">
 </head>
 <body>
+	<form action="/php/user/log_config.php" method="post" name="form_register" class="login-form">
 	<ul>
 		<li>
-			<label></label>
+			<label>Имя персонажа</label>
+			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+		</li>
+		<li>
+			<label>Фамилия Персонажа</label>
+			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+		</li>
+		<li>
+			<label>Прозвище персонажа</label>
+			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+		</li>
+		<li>
+			<label>Возраст персонажа</label>
+			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+		</li>
+		<li>
+			<label>Привычки персонажа</label>
+			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+		</li>
+		<li>
+			<label>Что любит персонажа</label>
+			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+		</li>
+		<li>
+			<label>Что не любит персонажа</label>
+			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+		</li>
+		<li>
+			<label>Пол персонажа</label>
+			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+		</li>
+		<li>
+			<label>Биография персонажа</label>
+			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+		</li>
+		<li>
+			<label>Страхи персонажа (если есть)</label>
+			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+		</li>
+		<li>
+			<label>Болезни персонажа (если есть)</label>
+			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
 		</li>
 	</ul>
+	</form>
 </body>
 </html>
