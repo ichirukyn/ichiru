7a5f493d41cf582b89838d60c497f6dfad7b3183
aoke-sudzuki <aoke.sudzuki@bk.ru>
2020-02-01 13:02:10 +0300

    structure repeare

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if (empty($_SESSION['user_class'])){    
-    include "D:/os/OSPanel/domains/ichiru/html/page/header.php";
+    include "D:/os/OSPanel/domains/ichiru/templates/header.php";
 }
 else{
     include "D:/os/OSPanel/domains/ichiru/php/page/main.php";
