59b2f517651bd49ff7921c52566af0d0186b203d
ichirukyn <55537819+ichirukyn@users.noreply.github.com>
2020-02-01 13:19:59 +0300

    `

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
 session_start();
-if (empty($_SESSION['user_class'])){    
-    include "D:/os/OSPanel/domains/ichiru/templates/header.php";
+if (empty($_SESSION['userlogin'])){    
+    include "D:/os/OSPanel/domains/ichiru/templates/modules/header.php";
 }
-else{
+else if (empty($_SESSION['userclass'])) {
+	include 'D:/os/OSPanel/domains/ichiru/templates/modules/createcharacter.php';
+}
+else
+{
     include "D:/os/OSPanel/domains/ichiru/php/page/main.php";
 }
 ?>
---
+++ templates/modules/createcharacter.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="/css/styles.css">
+</head>
+<body>
+	<ul>
+		<li>
+			<label></label>
+		</li>
+	</ul>
+</body>
+</html>
