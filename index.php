28f35dcb6974dd21511c369ef5b98bfe08516440
ichirukyn <55537819+ichirukyn@users.noreply.github.com>
2020-02-01 16:14:29 +0300

    индекс

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 include "D:/os/OSPanel/domains/ichiru/engine/constants.php";
-if (empty($_SESSION['user_class'])){
-    INC_Header();
+if (empty($_SESSION['userid'])){
+    include "D:/os/OSPanel/domains/ichiru/templates/modules/header.php";
 }
-else{
-    INC_Main();
+else if (empty($_SESSION['1userclass'])) {
+	include "D:/os/OSPanel/domains/ichiru/templates/modules/createcharacter.php";
 }
 ?>
--- templates/modules/createcharacter.php
+++ templates/modules/createcharacter.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="/css/styles.css">
+	<link rel="stylesheet" type="text/css" href="http://ichiru/css/styles.css">
 </head>
 <body>
 	<form action="/php/user/log_config.php" method="post" name="form_register" class="login-form">
@@ -12,43 +12,43 @@
 		</li>
 		<li>
 			<label>Фамилия Персонажа</label>
-			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+			<input type="text" name="" placeholder="Фамилия персонажа" class="input" data-validate>
 		</li>
 		<li>
 			<label>Прозвище персонажа</label>
-			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+			<input type="text" name="" placeholder="Прозвище персонажа" class="input" data-validate>
 		</li>
 		<li>
 			<label>Возраст персонажа</label>
-			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+			<input type="text" name="" placeholder="Возраст персонажа" class="input" data-validate>
 		</li>
 		<li>
 			<label>Привычки персонажа</label>
-			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+			<input type="text" name="" placeholder="Привычки персонажа" class="input" data-validate>
 		</li>
 		<li>
-			<label>Что любит персонажа</label>
-			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+			<label>Что любит персонаж</label>
+			<input type="text" name="" placeholder="Что любит персонаж" class="input" data-validate>
 		</li>
 		<li>
-			<label>Что не любит персонажа</label>
-			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+			<label>Что не любит персонаж</label>
+			<input type="text" name="" placeholder="Что не любит персонаж" class="input" data-validate>
 		</li>
 		<li>
 			<label>Пол персонажа</label>
-			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+			<input type="text" name="" placeholder="Пол персонажа" class="input" data-validate>
 		</li>
 		<li>
 			<label>Биография персонажа</label>
-			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+			<input type="text" name="" placeholder="Биография персонажа" class="input" data-validate>
 		</li>
 		<li>
 			<label>Страхи персонажа (если есть)</label>
-			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+			<input type="text" name="" placeholder="Страхи персонажа" class="input" data-validate>
 		</li>
 		<li>
 			<label>Болезни персонажа (если есть)</label>
-			<input type="text" name="" placeholder="Имя персонажа" class="input" data-validate>
+			<input type="text" name="" placeholder="Болезни персонажа" class="input" data-validate>
 		</li>
 	</ul>
 	</form>
